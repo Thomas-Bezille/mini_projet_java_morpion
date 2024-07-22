89f8f98b5516d3122f3d87e9f428b946fc519f52
thomas <bezille.thomasb@gmail.com>
2024-07-22 17:23:05 +0200

    📦 NEW: Error handling: possibility of adding names

--- src/App.java
+++ src/App.java
@@ -1,26 +1,29 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 import com.dyma.game.Player;
 import com.dyma.game.TicTacToe;
 import com.dyma.exceptions.TicTacToeInvalidInputException;
+import static com.dyma.game.StringConstants.BLANK;
 
 public class App {
     public static void main(String[] args) throws Exception {
         final var game = new TicTacToe();
 
         var player = Player.FIRST;
+        var players = initPlayers();
 
         while (true) {
             try {
                 System.out.println(game);
-                System.out.println(player + " / Saisisser un nombre en 1 et 9 : ");
+                System.out.println(players.get(player) + " / Saisisser un nombre en 1 et 9 : ");
                 final var inputUser = getInputUser();
 
                 game.processInput(player, inputUser);
 
                 if (game.checkWin()) {
                     System.out.println(game);
-                    System.out.println("Le joueur " + player + " a gagné !");
+                    System.out.println("Le joueur " + players.get(player) + " a gagné !");
                     break;
                 }
 
@@ -39,6 +42,21 @@ public class App {
         }
     }
 
+    private static HashMap<Player, String> initPlayers() {
+        var players = new HashMap<Player, String>();
+        var scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Saisir le nom du joueur 1 : ");
+            players.put(Player.FIRST, scanner.nextLine());
+        } while (players.get(Player.FIRST).equals(BLANK));
+        do {
+            System.out.println("Saisir le nom du joueur 2 : ");
+            players.put(Player.SECOND, scanner.nextLine());
+        } while (players.get(Player.SECOND).equals(BLANK));
+        return players;
+    }
+
     private static int getInputUser() throws TicTacToeInvalidInputException {
         final var scanner = new Scanner(System.in);
         var input = scanner.nextLine();
@@ -50,7 +68,6 @@ public class App {
             throw new TicTacToeInvalidInputException("Le chiffre doit être compris entre 1 et 9");
         }
         return inputEntier;
-
     }
 
     private static Player nextPlayer(Player player) {
--- src/com/dyma/game/StringConstants.java
+++ src/com/dyma/game/StringConstants.java
@@ -3,4 +3,5 @@ package com.dyma.game;
 public class StringConstants {
   public static final String LINE_SEPARATOR = System.lineSeparator();
   public static final String SPACE = " ";
+  public static final String BLANK = "";
 }
